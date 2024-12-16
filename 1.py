76726f0cfeef12ab9ac9ef6278e90a5267488665
vitarafavich <v.rafaevich@g.nsu.ru>
2024-12-16 15:54:22 +0700

    first commit

--- 1.py
+++ 1.py
@@ -1,1 +1,13 @@
+def dictionary_counting(sent):
+    dict = {}   #ключи это слова, а значения - количество их вхождений
+    for word in sent:
+        if word in dict:
+            dict[word] += 1
+        else:
+            dict[word] = 1
+    sorted_dict = sorted(dict, key=dict.get, reverse=True)
+    print(*sorted_dict)
 
+
+sentence = input('enter the words separated by a space: ').lower().split()
+dictionary_counting(sentence)
